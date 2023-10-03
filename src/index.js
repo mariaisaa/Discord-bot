5a4bb52533a3a767913e208b537c8734f321b6be
Maria Isabel <mariacifuentescc@gmail.com>
2023-10-03 11:33:08 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,  
+        IntentsBitField.Flags.GuildMembers, //to save who members are in the server
+        IntentsBitField.Flags.GuildMessages, //to lissent messages 
+        IntentsBitField.Flags.MessageContent, 
+    ],
+});
+
+
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is online.`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === 'hello') {
+        message.reply('Hey!');
+    }
+});
+
+client.login(process.env.TOKEN);

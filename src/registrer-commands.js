6f9253d297593af8fa6559c74b83f11d509f6c00
Maria Isabel <mariacifuentescc@gmail.com>
2023-10-06 16:31:56 -0500

    embeds

--- src/registrer-commands.js
+++ src/registrer-commands.js
@@ -3,44 +3,40 @@ const { REST, Routes, ApplicationCommandOptionType, } =  require('discord.js');
 
 const commands = [
     {
-    name: 'add',
-    description: 'Adds two numbers',
-    options: [
-        {
-            name: 'first-numbers',
-            description: 'the first number.',
-            type: ApplicationCommandOptionType.Number,
-            choices: [
-                {
-                    name: 'one',
-                    value: '1',
-                },
-                {
-                    name: 'two',
-                    value: '2',
-                },
-                {
-                    name: 'three',
-                    value: '3',
-                },
-            ],
-            required: true,
-        },
-        {
-            name: 'second-number',
-            description: 'the second number.',
-            type: ApplicationCommandOptionType.Number,
-            required: true,
-        }
+    name: 'embed',
+    description: 'Sends an embed',
+    // options: [
+    //     {
+    //         name: 'first-numbers',
+    //         description: 'the first number.',
+    //         type: ApplicationCommandOptionType.Number,
+    //         choices: [
+    //             {
+    //                 name: 'one',
+    //                 value: '1',
+    //             },
+    //             {
+    //                 name: 'two',
+    //                 value: '2',
+    //             },
+    //             {
+    //                 name: 'three',
+    //                 value: '3',
+    //             },
+    //         ],
+    //         required: true,
+    //     },
+    //     {
+    //         name: 'second-number',
+    //         description: 'the second number.',
+    //         type: ApplicationCommandOptionType.Number,
+    //         required: true,
+    //     }
 
-    ]
+    // ]
 
 },
 
-// {
-//     name: 'hey',
-//     description: 'Replies with hey!'
-// },
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
